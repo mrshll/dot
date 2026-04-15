0d8ba279ebc54ce3b9513e4927cc828611aa99f0
mrsll <m@mrshll.com>
2026-04-15 15:21:55 -0500

    sync: 2026-04-15 15:21

--- dot_config/fish/functions/listen.fish
+++ dot_config/fish/functions/listen.fish
@@ -1,5 +1,5 @@
 function listen --description "Stream MPD audio and open rmpc"
-    mpv --no-video http://$MPD_HOST:8000/mpd.ogg &
+    mpv --no-video --really-quiet http://$MPD_HOST:8000/mpd.ogg >/dev/null 2>&1 &
     set stream_pid $last_pid
     rmpc
     kill $stream_pid 2>/dev/null
